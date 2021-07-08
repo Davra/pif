dbeac34e1b617d4658d945a1df0dfa27b994e90c
Gerard Duffy <duffyg@gmail.com>
2021-07-08 23:11:07 +0100

    Add Prometheus client

--- index-biostar.js
+++ index-biostar.js
@@ -2,122 +2,89 @@
 
 const http = require('http')
 const httpProxy = require('http-proxy')
-// const prom = require('prom-client')
+const prom = require('prom-client')
 const port = 8080
+const svc = { tenantId: 'pif', microserviceName: 'biostar' }
+const proxyCount = new prom.Counter({
+    name: 'davra_api_microservice_proxy_count',
+    help: 'A count of proxy calls to microservices',
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode']
+})
+const proxyErrorCount = new prom.Counter({
+    name: 'davra_api_microservice_proxy_error_count',
+    help: 'A count of errors when proxying calls to microservices',
+    labelNames: ['tenantId', 'microserviceName']
+})
+const proxyRequestSize = new prom.Summary({
+    name: 'davra_api_microservice_proxy_request_size',
+    help: 'A summary of the quest sizes for proxy calls to microservices',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode'],
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
+})
+const proxyResponseSize = new prom.Summary({
+    name: 'davra_api_microservice_proxy_response_size',
+    help: 'A summary of the response sizes for proxy calls to microservices',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode'],
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
+})
+const proxyResponseTime = new prom.Summary({
+    name: 'davra_api_microservice_proxy_response_time',
+    help: 'A summary of the time taken for the microservice to complete the response',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99],
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode']
+})
+const register = new prom.Registry()
+register.registerMetric(proxyCount)
+register.registerMetric(proxyErrorCount)
+register.registerMetric(proxyRequestSize)
+register.registerMetric(proxyResponseSize)
+register.registerMetric(proxyResponseTime)
 
 const proxy = httpProxy.createProxyServer({})
 proxy.on('error', function (err, req, res) {
     console.error(err)
+    proxyErrorCount.labels(svc.tenantId, svc.microserviceName).inc()
     res.writeHead(500, {
         'Content-Type': 'text/plain'
     })
     res.end('Something went wrong: ' + err)
 })
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    req.davraStartTime = Date.now()
     console.log('BioStar request:', req.url)
 })
 proxy.on('proxyRes', function (proxyRes, req, res) {
-    console.log('BioStar response status:', proxyRes.statusMessage)
+    const duration = Date.now() - req.davraStartTime
+    proxyResponseTime.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(duration)
+    proxyCount.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).inc()
+    proxyRequestSize.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesRead)
+    proxyResponseSize.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesWritten)
+    console.log('BioStar response status:', proxyRes.statusCode, proxyRes.statusMessage)
 })
 
-/// ////////////////////////////////////////////////////////////////////////////
-//
-// Davra Secure Proxy for microservices
-//
-/// ////////////////////////////////////////////////////////////////////////////
-/*
-var proxy = httpProxy.createProxyServer();
-
-const proxyLookupTime = new prom.Summary({
-    name: 'davra_api_microservice_proxy_lookup_time',
-    help: 'A summary of the time taken to lookup the microservice route to proxy requests',
-    maxAgeSeconds: 900,
-    ageBuckets: 4,
-    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99],
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName']
-});
-
-const proxyResponseTime = new prom.Summary({
-    name: 'davra_api_microservice_proxy_response_time',
-    help: 'A summary of the time taken for the downstream microservice to complete the response (this does not include the time to lookup the downstream microservice)',
-    maxAgeSeconds: 900,
-    ageBuckets: 4,
-    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99],
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName', 'method', 'statusCode']
-});
-
-const proxyCount = new prom.Counter({
-    name: 'davra_api_microservice_proxy_count',
-    help: 'A count of proxy calls to microservices',
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName', 'method', 'statusCode']
-});
-
-const proxyErrorCount = new prom.Counter({
-    name: 'davra_api_microservice_proxy_error_count',
-    help: 'A count of errors when proxying calls to microservices',
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName']
-});
-
-const proxyRequestSize = new prom.Summary({
-    name: 'davra_api_microservice_proxy_request_size',
-    help: 'A summary of the quest sizes for proxy calls to microservices',
-    maxAgeSeconds: 900,
-    ageBuckets: 4,
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName', 'method', 'statusCode'],
-    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
-});
-
-const proxyResponseSize = new prom.Summary({
-    name: 'davra_api_microservice_proxy_response_size',
-    help: 'A summary of the response sizes for proxy calls to microservices',
-    maxAgeSeconds: 900,
-    ageBuckets: 4,
-    labelNames: ['tenantId', 'microserviceUUID', 'microserviceName', 'method', 'statusCode'],
-    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
-});
-
-function doProxy (req, res, svc, prefix) {
-    const startTime = Date.now();
-
-    var target = util.format('http://%s:%d%s', svc.ip, svc.port, req.url);
-
-    var opts = { target: target, ignorePath: true };
-    if (req.user) {
-        opts.headers = {
-            'x-user-id': req.user.id || req.user.UUID,
-            'x-user-uuid': req.user.UUID
-        };
+const server = http.createServer(async function (req, res) {
+    if (req.method === 'GET' && req.url === '/metrics') {
+        res.writeHead(200, { 'Content-Type': register.contentType })
+        res.end(await register.metrics())
     }
-
-    // record proxy metrics when response is finished
-    res.on('finish', () => {
-        const duration = Date.now() - startTime;
-        proxyResponseTime.labels(svc.tenantId, svc.microserviceUUID, svc.microserviceName, req.method, res.statusCode).observe(duration);
-        proxyCount.labels(svc.tenantId, svc.microserviceUUID, svc.microserviceName, req.method, res.statusCode).inc();
-        proxyRequestSize.labels(svc.tenantId, svc.microserviceUUID, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesRead);
-        proxyResponseSize.labels(svc.tenantId, svc.microserviceUUID, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesWritten);
-    });
-
-    proxy.web(req, res, opts, function (err) {
-        if (err) {
-            console.error(err);
-            proxyErrorCount.labels(svc.tenantId, svc.microserviceUUID, svc.microserviceName).inc();
-        }
-        try {
-            res.status(202).json({ status: 'error', message: 'Failed to forward request' });
-        } catch (ex) {
-            console.error(ex);
-        }
-    });
-}
-*/
-
-const server = http.createServer(function (req, res) {
-    // https://10.0.33.35/api/login
-    proxy.web(req, res, {
-        changeOrigin: true,
-        target: 'https://10.0.33.35'
-    })
+    else if (req.method === 'GET' && req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('')
+    }
+    else {
+        // https://10.0.33.35/api/login
+        proxy.web(req, res, {
+            changeOrigin: true,
+            target: 'https://10.0.33.35',
+            secure: false
+        })
+    }
 })
 console.log(`BioStar proxy listening on port ${port}`)
 server.listen(port)
--- index-embrava.js
+++ index-embrava.js
@@ -1,28 +1,90 @@
+'use strict'
+
 const http = require('http')
 const httpProxy = require('http-proxy')
+const prom = require('prom-client')
 const port = 8080
+const svc = { tenantId: 'pif', microserviceName: 'biostar' }
+const proxyCount = new prom.Counter({
+    name: 'davra_api_microservice_proxy_count',
+    help: 'A count of proxy calls to microservices',
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode']
+})
+const proxyErrorCount = new prom.Counter({
+    name: 'davra_api_microservice_proxy_error_count',
+    help: 'A count of errors when proxying calls to microservices',
+    labelNames: ['tenantId', 'microserviceName']
+})
+const proxyRequestSize = new prom.Summary({
+    name: 'davra_api_microservice_proxy_request_size',
+    help: 'A summary of the quest sizes for proxy calls to microservices',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode'],
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
+})
+const proxyResponseSize = new prom.Summary({
+    name: 'davra_api_microservice_proxy_response_size',
+    help: 'A summary of the response sizes for proxy calls to microservices',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode'],
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99]
+})
+const proxyResponseTime = new prom.Summary({
+    name: 'davra_api_microservice_proxy_response_time',
+    help: 'A summary of the time taken for the microservice to complete the response',
+    maxAgeSeconds: 900,
+    ageBuckets: 4,
+    percentiles: [0.25, 0.50, 0.75, 0.9, 0.95, 0.99],
+    labelNames: ['tenantId', 'microserviceName', 'method', 'statusCode']
+})
+const register = new prom.Registry()
+register.registerMetric(proxyCount)
+register.registerMetric(proxyErrorCount)
+register.registerMetric(proxyRequestSize)
+register.registerMetric(proxyResponseSize)
+register.registerMetric(proxyResponseTime)
 
 const proxy = httpProxy.createProxyServer({})
 proxy.on('error', function (err, req, res) {
     console.error(err)
+    proxyErrorCount.labels(svc.tenantId, svc.microserviceName).inc()
     res.writeHead(500, {
         'Content-Type': 'text/plain'
     })
     res.end('Something went wrong: ' + err)
 })
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
+    req.davraStartTime = Date.now()
     console.log('Embrava request:', req.url)
 })
 proxy.on('proxyRes', function (proxyRes, req, res) {
-    console.log('Embrava response status:', proxyRes.statusMessage)
+    const duration = Date.now() - req.davraStartTime
+    proxyResponseTime.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(duration)
+    proxyCount.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).inc()
+    proxyRequestSize.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesRead)
+    proxyResponseSize.labels(svc.tenantId, svc.microserviceName, req.method, res.statusCode).observe(req.socket.bytesWritten)
+    console.log('Embrava response status:', proxyRes.statusCode, proxyRes.statusMessage)
 })
-const server = http.createServer(function (req, res) {
-    // https://eusfuncapp01.azurewebsites.net/api/Authentication
-    // https://pif.davra.com/microservices/wrld3d/api/Authentication
-    proxy.web(req, res, {
-        changeOrigin: true,
-        target: 'https://eusfuncapp01.azurewebsites.net'
-    })
+const server = http.createServer(async function (req, res) {
+    if (req.method === 'GET' && req.url === '/metrics') {
+        res.writeHead(200, { 'Content-Type': register.contentType })
+        res.end(await register.metrics())
+    }
+    else if (req.method === 'GET' && req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('')
+    }
+    else {
+        // https://eusfuncapp01.azurewebsites.net/api/Authentication
+        // https://pif.davra.com/microservices/wrld3d/api/Authentication
+        proxy.web(req, res, {
+            changeOrigin: true,
+            target: 'https://eusfuncapp01.azurewebsites.net',
+            secure: false
+        })
+    }
 })
 console.log(`Embrava proxy listening on port ${port}`)
 server.listen(port)
